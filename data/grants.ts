1e93d04ead040205fe4a04104f1dfd431e3dec91
sha-ce <ukita.kosuke00@gmail.com>
2026-04-24 23:03:26 +0900

    update.

--- data/grants.ts
+++ data/grants.ts
@@ -1,9 +1,16 @@
 export const grants = [
   { 
+    name: "International Travel Grant (海外渡航旅費援助)", 
+    url: "https://www.taf.or.jp/grant-b/02/",
+    year: "Apr. 2026", 
+    organization: "The Telecommunications Advancement Foundation",
+    description: "JPY 290,000. Morroco, Tangier",
+  },
+  { 
     name: "Kyushu Institute of Technology SPRING Scholarship Awarde", 
     url: "https://www.ccr.kyutech.ac.jp/dc_support/spring",
     year: "Apr. 2026 - Mar. 2029", 
     organization: "Japan Science and Technology Agency (JST)",
-    description: "JPMJSP2154",
+    description: "JPMJSP2154, 0073",
   },
 ]
